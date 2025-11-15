f47cc5dbd3eb731249e88dcd117c552b170106d9
Austinet <austinetsplendour@gmail.com>
2025-11-15 23:53:49 +0100

    ft: adds swiper slide feature for the doctors section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700", "800", "900"],
